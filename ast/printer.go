1ddb752b5355bcacd8d93d048e354ab1e30977fd
IsaacHdezA <aldoisaachernandez@outlook.com>
2026-01-05 23:26:01 -0600

    [PARSER]: Challenge Reverse Polish Notation Printer

--- ast/printer.go
+++ ast/printer.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 )
 
+// Pretty Printer
 type AstPrinter struct{}
 
 func (p AstPrinter) VisitBinaryExpr(expr BinaryExpr) any {
@@ -41,4 +42,28 @@ func (p AstPrinter) parentezize(name string, exprs ...Expr) string {
 	output += ")"
 
 	return output
+}
+
+// Reverse Polish Notation Printer
+type AstPrinterRPN struct{}
+
+func (p AstPrinterRPN) VisitBinaryExpr(expr BinaryExpr) any {
+
+	return fmt.Sprintf("%s %s %s", expr.Left.Accept(p), expr.Right.Accept(p), expr.Operator.Lexeme)
+}
+
+func (p AstPrinterRPN) VisitGroupingExpr(expr GroupingExpr) any {
+	return fmt.Sprintf("%s", expr.Expression.Accept(p))
+}
+
+func (p AstPrinterRPN) VisitLiteralExpr(expr LiteralExpr) any {
+	return fmt.Sprintf("%v", expr.Value)
+}
+
+func (p AstPrinterRPN) VisitUnaryExpr(expr UnaryExpr) any {
+	return fmt.Sprintf("%v%s", expr.Right.Accept(p), expr.Operator.Lexeme)
+}
+
+func (p AstPrinterRPN) Print(expr Expr) string {
+	return expr.Accept(p).(string)
 }
--- test/print_test.go
+++ test/print_test.go
@@ -7,7 +7,7 @@ import (
 	"github.com/IsaacHdezA/glox/token"
 )
 
-func TestPrint(t testing.T) {
+func TestPrint(t *testing.T) {
 	expr := ast.BinaryExpr{
 		Left: ast.UnaryExpr{
 			Operator: token.Token{Type: token.MINUS, Lexeme: "-"},
@@ -22,6 +22,34 @@ func TestPrint(t testing.T) {
 	printer := ast.AstPrinter{}
 	output := printer.Print(expr)
 	correct := "(* (- 123) (group 45.67))"
+
+	if output != correct {
+		t.Fatalf("Output %q failed. Expecting %q", output, correct)
+	}
+}
+
+func TestPrintRPN(t *testing.T) {
+	expr := ast.BinaryExpr{
+		Left: ast.GroupingExpr{
+			Expression: ast.BinaryExpr{
+				Left:     ast.LiteralExpr{Value: 1},
+				Operator: token.Token{Type: token.PLUS, Lexeme: "+"},
+				Right:    ast.LiteralExpr{Value: 2},
+			},
+		},
+		Operator: token.Token{Type: token.STAR, Lexeme: "*"},
+		Right: ast.GroupingExpr{
+			Expression: ast.BinaryExpr{
+				Left:     ast.LiteralExpr{Value: 4},
+				Operator: token.Token{Type: token.MINUS, Lexeme: "-"},
+				Right:    ast.LiteralExpr{Value: 3},
+			},
+		},
+	}
+
+	printer := ast.AstPrinterRPN{}
+	output := printer.Print(expr)
+	correct := "1 2 + 4 3 - *"
 
 	if output != correct {
 		t.Fatalf("Output %q failed. Expecting %q", output, correct)
